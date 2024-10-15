ad732b1158acb9c31e65623c3e871429ddaccb63
XangVk <xang.vk@gmail.com>
2024-10-15 11:44:09 +0700

    uploadfile

--- src/pages/homemain.js
+++ src/pages/homemain.js
@@ -41,8 +41,9 @@ export default function Homemain() {
 
                         <div className="main-slidebar">
                             <div className="icon-menu iconmenu  w-100 bg-white  d-flex">
-                                <span> soap data phonenumber </span>
-                                <i className="fa fa-chevron-left color-white border-t-l-radius-15-px  border-b-l-radius-15-px bg-default"></i>
+                                <span className="m-auto"> soap query phone {/*soap data phonenumber*/} </span> 
+
+                                <i className="fa fa-chevron-left color-white border-t-l-radius-15-px  border-b-l-radius-15-px bg-default border-white"></i>
                             </div>
 
                             <div className="nav-link">
