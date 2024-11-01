02a1a84b2791a84796c8516476be129337823638
XangVk <xang.vk@gmail.com>
2024-11-01 14:13:27 +0700

    uploadfile

--- src/pages/homemain.js
+++ src/pages/homemain.js
@@ -49,7 +49,7 @@ export default function Homemain() {
                             </div>
 
                             <div className="nav-link">
-                                <li>
+                                <li className={`${path == "/inquery" ? "bg-menubar" : ""}`}>
                                     <a href="/inquery">
                                         <i className="fa fa-newspaper-o"> </i>
                                         <span > inquery phone </span>
@@ -57,19 +57,19 @@ export default function Homemain() {
                                     </a>
 
                                 </li>
-                                <li>
+                                <li className={`${path == "/modifielddatetime" ? "bg-menubar" : ""}`}>
                                     <a href="/modifielddatetime">
                                         <i className="fa fa-calendar"> </i>
                                         <span > modify datetime </span>
                                     </a>
                                 </li>
-                                <li>
+                                <li className={`${path == "/Modifieldlistphone" ? "bg-menubar" : ""}`}>
                                     <a href="/Modifieldlistphone">
                                         <i className="fa fa-calendar-plus-o"> </i>
                                         <span > modify list datetime </span>
                                     </a>
                                 </li>
-                                <li className="card-item-add">
+                                <li className={`${path == "/Addpackagephone" || path == "/Addpackagelistphone" ? "bg-menubar" : ""} card-item-add`}>
                                     <div className="d-flex align-items-center">
                                         <a >
                                             <i className="fa fa-id-card-o"> </i>
@@ -78,19 +78,18 @@ export default function Homemain() {
                                         <i className="fa fa-chevron-down arrow"> </i>
                                     </div>
                                     <ul className="link-submenu">
-                                        <li> <a href="/Addpackagephone"> add package phone</a> </li>
-                                        <li> <a href="/Addpackagelistphone"> add package listphone</a> </li>
+                                        <li style={{ backgroundColor: `${path == "/Addpackagephone" ? "#FFFFFF5c" : ""}` }} > <a href="/Addpackagephone"> add package phone</a> </li>
+                                        <li style={{ backgroundColor: `${path == "/Addpackagelistphone" ? "#FFFFFF5c" : ""}` }} > <a href="/Addpackagelistphone"> add package listphone</a> </li>
                                     </ul>
                                 </li>
-                                <li>
+                                <li className={`${path == "/changemaxdate" ? "bg-menubar" : ""}`}>
                                     <a href="/changemaxdate">
                                         <i className="fa fa-bars"> </i>
                                         <span > changemax date </span>
                                     </a>
                                 </li>
 
-                                <li className="card-item-detail"
-                                >
+                                <li className={`${path == "/desclogpage" ? "bg-menubar" : ""} card-item-detail`}>
                                     <div className="d-flex align-items-center justify-content-between ">
                                         <a >
                                             <i className="fa fa-file-text-o"> </i>
@@ -100,51 +99,51 @@ export default function Homemain() {
                                     </div>
                                     <ul className="link-submenu mb-0">
 
-                                        <li
-                                            onClick={() => {
-                                                console.log("page");
-                                                setpage(true)
-                                            }}
+                                        <li 
+                                        onClick={() => {
+                                            console.log("page");
+                                            setpage(true)
+                                        }}
                                         > <a > description page </a> </li>
-                                        <li > <a href="/desclogpage"> detaillog page </a> </li>
+                                <li style={{backgroundColor :` ${path == "/desclogpage" ? "#FFFFFF5c" : ""}`}}> <a href="/desclogpage"> detaillog page </a> </li>
 
-                                    </ul>
-                                </li>
-
-                            </div>
-
-
-                        </div>
-                        <div className="home-section">
-                            <div className="w-100 bg-default h-50-px d-flex align-items-center px-3">
-                                <i className="fa fa-home color-white f-24-px"></i><span className="color-white ml-2 f-20-px"> Home : {path.replace("/", "").toLocaleLowerCase()} </span>
-                            </div>
-                            <BrowserRouter>
-
-                                <Routes>
-                                    <Route path="/inquery" element={<Inquery />} />
-                                    <Route path="/modifielddatetime" element={<Modifielddatetime />} />
-                                    <Route path="/modifieldlistphone" element={<Modifieldlistphone />} />
-                                    <Route path="/Addpackagephone" element={<Addpackagephone />} />
-                                    <Route path="/Addpackagelistphone" element={<Addpackagelistphone />} />
-                                    <Route path="/Descriptionpage" element={<Descriptionpage />} />
-                                    <Route path="/changemaxdate" element={<ChangMaxDate />} />
-                                    <Route path="/desclogpage" element={<Descriptionlogpage />} />
-                                </Routes>
-                            </BrowserRouter>
-
-                        </div>
+                            </ul>
+                        </li>
 
                     </div>
+
+
+                        </div >
+        <div className="home-section">
+            <div className="w-100 bg-default h-50-px d-flex align-items-center px-3">
+                <i className="fa fa-home color-white f-24-px"></i><span className="color-white ml-2 f-20-px"> Home : {path.replace("/", "").toLocaleLowerCase()} </span>
+            </div>
+            <BrowserRouter>
+
+                <Routes>
+                    <Route path="/inquery" element={<Inquery />} />
+                    <Route path="/modifielddatetime" element={<Modifielddatetime />} />
+                    <Route path="/modifieldlistphone" element={<Modifieldlistphone />} />
+                    <Route path="/Addpackagephone" element={<Addpackagephone />} />
+                    <Route path="/Addpackagelistphone" element={<Addpackagelistphone />} />
+                    <Route path="/Descriptionpage" element={<Descriptionpage />} />
+                    <Route path="/changemaxdate" element={<ChangMaxDate />} />
+                    <Route path="/desclogpage" element={<Descriptionlogpage />} />
+                </Routes>
+            </BrowserRouter>
+
+        </div>
+
+                    </div >
                     :
-                    <>
-                        <div className="w-100 h-100-vh bg-gray">
-                            <Descriptionpage callpages={callpage} />
-                        </div>
+    <>
+        <div className="w-100 h-100-vh bg-gray">
+            <Descriptionpage callpages={callpage} />
+        </div>
 
 
-                    </>
-            }
+    </>
+}
         </>
 
 
