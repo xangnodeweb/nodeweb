3d7142f4d36c0aea2428a7d767a4a843fa0fc741
XangVk <xang.vk@gmail.com>
2024-11-15 11:29:46 +0700

    uploadfile

--- apppath.js
+++ apppath.js
@@ -2,21 +2,21 @@ const express = require("express");
 
 const app = express();
 const path = require("path");
-
+const cors = require("cors");
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
-
+app.use(cors());
 app.use("/image", express.static(path.join(__dirname, "image")));
 
-app.get("/" , async (req ,res) => {
-try {
-    
-    return res.status(200).json("status : OK")
-} catch (error) {
-    console.log(error)
-}
+app.get("/", async (req, res) => {
+    try {
+
+        return res.status(200).json("status : OK")
+    } catch (error) {
+        console.log(error)
+    }
 
 })
 
