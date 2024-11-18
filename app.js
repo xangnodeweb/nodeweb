625139a10b74e567c1f89aafe1184d1fead6a494
XangVk <xang.vk@gmail.com>
2024-11-18 09:25:30 +0700

    uploadfiles

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const path = require("path");
 const app = express();
 
 const cors = require("cors");
+const https = require("https");
+const fs = require("fs");
+
 const apirouter = require("./controllerapi");
 const apichangerouter = require("./controllerchangeapi");
 
@@ -12,6 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3000;
+const PORTS = process.env.PORT_APP || 3001;
 
 app.use(express.static("dist"));
 
@@ -19,10 +23,18 @@ app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname + "/dist/index.html"))
 });
 app.use("/api", apirouter);
-app.use("/apichangemain" , apichangerouter);
+app.use("/apichangemain", apichangerouter);
 // app.use("/image" , express.static(path.join(__dirname , "image")))
 
 app.listen(PORT, () => {
     console.log("running app is port on : " + PORT)
-
 })
+
+const options = {
+    key: fs.readFileSync("server.key"),
+    cert: fs.readFileSync("server.cert")
+}
+
+https.createServer(options, app).listen(PORTS, (req, res) => {
+    console.log("running is port : " + PORTS);
+});
