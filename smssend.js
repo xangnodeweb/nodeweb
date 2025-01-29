7f3ce1d6aa537be3d276f324670f5c6ff753a6c4
XangVk <xang.vk@gmail.com>
2025-01-29 11:49:23 +0700

    upload

--- smssend.js
+++ smssend.js
@@ -52,36 +52,34 @@ app.post("/addpackagesms", async (req, res) => {
     try {
 
         const body = req.body;
-        // Prepaid_Staff_10GB 
-
         const reqaddpackage = bodyaddpackage(); // body request add package
 
-
+        let model = [];
+        let modelbody = [];
+        let modelresponse = {};
+        let modelInfo = [];
         if (body.length > 0) {
 
             for (var i = 0; i < body.length; i++) {
 
                 console.log(body[i])
-                const reqaddpackage = bodyaddpackage(body[i].phone, body[i].packagename, body[i].datestart, body[i].dateend, body[i].refillstoptime); // body request add package
-
-                //   console.log(reqaddpackage)
+                const bodyaddpackages = await bodyaddpackage(body[i].phone, body[i].packagename, body[i].datestart, body[i].dateend, body[i].refillstoptime); // body request add package
 
                 const headers = {
                     'Content-Type': 'text/xml;charset=utf-8'
                 }
-                let model = [];
-                let modelbody = [];
-                let modelresponse = {};
+
 
                 await fetch("http://10.0.10.35/vsmpltc/web/services/amfwebservice.asmx", {
                     method: "POST",
                     headers: headers,
-                    body: reqaddpackage
+                    body: bodyaddpackages
                 }).then(response => {
                     return response.text();
                 }).then(responseText => {
 
                     const modeldata = responseText;
+                    // console.log(modeldata)
 
                     parseString(modeldata, function (err, result) {
                         let data = JSON.stringify(result);
@@ -90,63 +88,57 @@ app.post("/addpackagesms", async (req, res) => {
                         model.push(datas['soap:Envelope']['soap:Body'])
                         const responsesuccess = datas['soap:Envelope']['soap:Body'][0]['AddCounterResponse'][0]['AddCounterResult'][0]['OperationStatus'][0]; // operation status
                         const countersuccess = datas['soap:Envelope']['soap:Body'][0]['AddCounterResponse'][0]['AddCounterResult'][0]['CounterArray'][0]['CounterInfo']; // counterinfo data
+                      
                         if (responsesuccess.IsSuccess[0] == 'true') {
-                            let modelInfo = [];
-
                             if (countersuccess.length > 0) {
                                 for (var i = 0; i < countersuccess.length; i++) {
-
-                                    const data = { Msisdn: countersuccess[i].Msisdn[0], ProductNumber: countersuccess[i].ProductNumber[0], CounterName: countersuccess[i].CounterName[0], StartTime: countersuccess[i].StartTime[0], ExpiryTime: countersuccess[i].ExpiryTime[0], status: responsesuccess.IsSuccess[0] };
+                                    const data = { Msisdn: countersuccess[i].Msisdn[0], ProductNumber: countersuccess[i].ProductNumber[0], CounterName: countersuccess[i].CounterName[0], StartTime: countersuccess[i].StartTime[0], ExpiryTime: countersuccess[i].ExpiryTime[0], status: responsesuccess.IsSuccess[0], code: responsesuccess.Code[0], message: responsesuccess.Description[0] };
                                     modelInfo.push(data)
                                 }
                             }
-                            const response = {
-                                "status": true,
-                                "message": "success",
-                                "code": 0,
-                                "result": modelInfo
-                            }
-                            modelresponse = response;
-
-                            // console.log(modelInfo);
                         } else {
-                            const data = { status: responsesuccess.IsSuccess[0], Code: responsesuccess.Code[0], Msisdn: body[i].phone, ProductNumber: "not found data", CounterName: "not found data", StartTime: "not found data", ExpiryTime: "not found data", message: responsesuccess.Description[0] };
-                            const response = {
-                                "status": false,
-                                "message": "failed",
-                                "code": 1,
-                                "result": data
-                            }
-                            modelresponse = response;
+                            const data = { Msisdn: body[i].phone, ProductNumber: "not found data", CounterName: "not found data", StartTime: "not found data", ExpiryTime: "not found data", status: responsesuccess.IsSuccess[0], code: responsesuccess.Code[0], message: responsesuccess.Description[0] };
+                            modelInfo.push(data)
                         }
-                    })
-                    if (responsesuccess.status = true) {
+                    });
 
-                        adddatafile(responsesuccess, 0);
-                        return res.status(200).json(responsesuccess);
-                    } else {
-                        return res.status(400).json(responsesuccess);
-                    }
                 }).catch(err => {
                     const error = JSON.stringify(err);
                     const errors = JSON.parse(error);
                     if (err) {
                         if (errors.code == "ETIMEDOUT") {
-                            modelresponse = { status: false, code: 2, message: "cannot add package ConnectTimeoutError", result: null }
+                            const data = { Msisdn: body[0].phone, ProductNumber: "not found data", CounterName: "not found data", StartTime: "not found data", ExpiryTime: "not found data", status: false, code: 2, message: "cannot add package ConnectTimeoutError" };
+                            modelInfo.push(data);
+
                         }
                     }
 
-                })
-            
+                });
+                const index = modelInfo.findIndex(x => x.status == false && x.code == 2);
+                if (index != -1) {
+                    break;
+                }
 
+            }
 
+            const idnexresponse = modelInfo.filter(x => x.status == false && x.code == 2);
+
+            if (idnexresponse.length == 0) {
+
+                return res.status(200).json({ status: true, code: 0, message: "add package success", result: modelInfo })
+
+            } else {
+
+                const status = idnexresponse.length == 0 ? 2 : 1;
+                const message = idnexresponse.length > 0 ? "cannot add package data ConnectionTimeOutError" : "cannot add package data";
+                return res.status(400).json({ status: false, code: status, message: message, result: modelInfo });
             }
 
 
         }
-
+        return
         console.log(body);
-        return res.status(200).json({ status: true, code: 0, message: 'addpackage_success', result: [] });
+        return res.status(200).json({ status: true, code: 0, message: 'addpackage_success', result: modelresponse });
 
     } catch (error) {
         console.log(error);
