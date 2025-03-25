ee2f57f04e32692aa0632f4726bb4f6f14d6e952
XangVk <xang.vk@gmail.com>
2025-03-25 15:45:04 +0700

    upload

--- smsfilesend.js
+++ smsfilesend.js
@@ -182,8 +182,9 @@ const logsendsms = async (data, smid, userid) => {
 
 
         const paths = path.join(__dirname, "./filedatatxt/")
-
-        let line = `${data.Msisdn}|${data.msgcontent}|${smid}|${data.statussms}|`;
+        let date = new Intl.DateTimeFormat("en-US", { day: "2-digit", month: "2-digit", year: "numeric", hour: "2-digit", minute: "2-digit", second: "2-digit", timeZone: "Asia/Bangkok" }).format(new Date());
+        date = date.toString().slice(0, 10);
+        let line = `${data.Msisdn}|${data.msgcontent}|${smid}|${data.statussms}|${date}\n`;
         await fs.appendFile(paths + "filesmscontent.txt", line, (err) => {
 
             if (err) {
